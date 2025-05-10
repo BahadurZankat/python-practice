b735902f3580c0ce50d3c407d04af2adbaadd433
Bahadur <bahadur@Bahadurs-MacBook-Air.local>
2025-05-10 21:41:06 +0530

    Minor changes are done

--- basicClassExample.py
+++ basicClassExample.py
@@ -4,3 +4,9 @@ class Student:
 
 s1=Student("Bahadur")
 print("Hey my name is" + s1.name)
+s2=Student("Zankat")
+print("Hey my name is" + s2.name)
+s3=Student("Raju")
+print("Hey my name is" + s3.name)
+s4=Student("Zala")
+print("Hey my name is" + s4.name)

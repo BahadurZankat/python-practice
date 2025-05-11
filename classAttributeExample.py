e6d8c741c20a24082ac368ab772faf3a31b82554
Bahadur <bahadur@Bahadurs-MacBook-Air.local>
2025-05-11 12:28:25 +0530

    tes_1t

--- classAttributeExample.py
+++ classAttributeExample.py
@@ -1,5 +1,6 @@
 class Student:
     college_name="L E College"
+    name="claaa name"
 
     def __init__(self,name):
         self.name=name
@@ -11,3 +12,9 @@ print(s1.college_name)
 s2=Student("Gandhi")
 print(s2.name)
 print(s2.college_name)
+
+#here , object attribute has higher precedence then class attaribute hence the output is
+#Rahul
+#L E College
+#Gandhi
+#L E College

e2c3953dd9520df3e25e00b0dc41f58518812c8c
Bahadur <bahadur@Bahadurs-MacBook-Air.local>
2025-05-11 12:25:37 +0530

    test

---
+++ classAttributeExample.py
@@ -0,0 +1,13 @@
+class Student:
+    college_name="L E College"
+
+    def __init__(self,name):
+        self.name=name
+s1=Student("Rahul")
+print(s1.name)
+print(s1.college_name)
+
+
+s2=Student("Gandhi")
+print(s2.name)
+print(s2.college_name)

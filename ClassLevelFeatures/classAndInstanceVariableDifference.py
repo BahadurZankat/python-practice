c6e2631eda9d3388cd63ea0b9dc9ec3ac497d13f
Bahadur <bahadur@Bahadurs-MacBook-Air.local>
2025-05-24 21:36:25 +0530

    class variables

--- ClassLevelFeatures/classAndInstanceVariableDifference.py
+++ ClassLevelFeatures/classAndInstanceVariableDifference.py
@@ -4,17 +4,17 @@ class Person:
     def changeName(self,name):
         self.name=name
 
-    def changeClassName(self,name):
+    def changeClassLevelVariableNameMethod1(self,name):
         Person.name=name
-    def changeClassNameMethod2(self,name):
+    def changeClassLevelVariableNameMethod2(self,name):
         self.__class__.name=name    
 
 p1=Person()
 p1.changeName("shradhha")
 print(p1.name) # This will represent object variable name  [output is => shradhha]
 print(Person.name) # This will represent class level variable name  [output is => Anynoumous]
-p1.changeClassName("khapra")
+p1.changeClassLevelVariableNameMethod1("khapra")
 print(Person.name) # This will represent class level variable name  [output is => khapra]
-p1.changeClassName("khapra method 2")
+p1.changeClassLevelVariableNameMethod2("khapra method 2")
 print(Person.name) # This will represent class level variable name  [output is => khapra method 2]
 

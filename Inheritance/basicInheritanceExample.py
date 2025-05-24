9e02fd3d23eca28039f9af40199bc922883bc5ef
Bahadur <bahadur@Bahadurs-MacBook-Air.local>
2025-05-24 18:41:37 +0530

    basic example of inhertance has been added

---
+++ Inheritance/basicInheritanceExample.py
@@ -0,0 +1,19 @@
+class Car:
+    colour="black"
+
+    @staticmethod
+    def start():
+        print("Car Started")
+
+    def stop():
+        print("car Stopped")    
+
+class Toyota(Car):
+    def __init__(self,name):
+        self.name=name
+
+
+car1=Toyota("My Car")
+print(car1.name)  # used own propert
+print(car1.colour) # Used Parent Property
+car1.start() # Used Parent Method
